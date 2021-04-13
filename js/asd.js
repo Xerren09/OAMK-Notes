85f769520cb5e2a7e5f3239fcc99e1650d1ea5ff
rainov <70270934+rainov@users.noreply.github.com>
2021-04-13 22:54:46 +0300

    transition between pages and minor fixes

---
+++ js/asd.js
@@ -0,0 +1,5 @@
+let homeworkHeaderButton = document.querySelector('#homeworkHeaderButton');
+homeworkHeaderButton.onlcick = () => {
+    //rightSideContainer.style.opacity = '0';
+    console.log('sdsads');
+};
