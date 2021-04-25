6801e894635272a6f027fb9921d2884f263c48d0
kaiora199 <71007877+kaiora199@users.noreply.github.com>
2021-04-25 21:44:12 +0300

    Added code.
    Added functionality for the user info changing.

---
+++ OAMK-Notes-TavisBranch/js/userPage.js
@@ -0,0 +1,19 @@
+groupButton.onclick = () => {
+    let userGroupChange = document.getElementById('groupCodeTarget');
+    let userGroupButton = document.getElementById('groupButton');
+    userGroupChange.innerHTML = `<input type="text" class="inputFieldHW" placeholder="Group code">`;
+    userGroupButton.innerHTML = `Save`;
+        /*groupButton.onclick = () => {
+
+    }*/
+}
+
+passwordButton.onclick = () => {
+    let passwordChange = document.getElementById('userPasswordTarget');
+    let userPassButton = document.getElementById('passwordButton');
+    passwordChange.innerHTML = `<input type="password" class="inputFieldHW" placeholder="Password"> <lf> <input type="password" class="inputFieldHW" placeholder="Retype your password">`;
+    userPassButton.innerHTML = `Save`;
+    /*passwordButton.onclick = () => {
+
+    }*/
+}
